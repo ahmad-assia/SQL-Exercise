68db629d5f69f2880498d29873985b1781d3b328
assia ahmad <ahmadassia676@gmail.com>
2021-12-13 14:23:45 +0200

    basic queries

---
+++ answers.sh
@@ -0,0 +1,19 @@
+\\1
+SELECT Name FROM students;;
+\\2
+SELECT * FROM students WHERE  age > 30;
+\\3
+SELECT Name FROM students WHERE  gender = "F" and age = 30;
+\\4
+SELECT points FROM students WHERE  name ="Alex";
+\\5
+INSERT INTO students VALUES ("13","Assia",27,"F",20);
+\\6
+UPDATE  students SET Points = Points+1 WHERE name ="Alex";
+\\7
+UPDATE  students SET Points = Points-1 WHERE  name ="Alex";
+
+
+
+
+
